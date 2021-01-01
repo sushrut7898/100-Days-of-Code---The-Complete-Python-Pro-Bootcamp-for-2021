bd254a310d01223baf2a9f5ba31b6febcbdd4cb4
sushrut7898 <sushrut7898@gmail.com>
2021-01-01 19:16:45 +0530

    first commit

---
+++ Day 1/basics.py
@@ -0,0 +1,48 @@
+print('Hello World!')       # Print String
+
+print('Day 1 - Python Print Function')
+print('The function is declared like this:')
+print('print(\'what to print\')')
+print("print('what to print')")
+
+print('Hello world!\nHello world!\nHello world!')
+print('Hello' + ' ' +'Sushrut')
+
+#1. Missing double quotes before the word Day.
+print("Day 1 - String Manipulation")
+
+#2. Outer double quotes changed to single quotes.
+print('String Concatenation is done with the "+" sign.')
+
+#3. Extra indentation removed
+print('e.g. print("Hello " + "world")')
+
+#4. Extra ( in print function removed.
+print("New lines can be created with a backslash and n.")
+
+name = input('What is your name?')      # INPUT Function
+print(name)
+
+print('Hello ' + input('What is your name?'))
+
+print(len(input("What is your name? ")))
+
+# Variables
+
+name = input('What is your name?')
+name = 'Sushrut'
+print(name)
+
+# 12 Challenge
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b)
+
+user_name = 'Sushrut Patil'
+userName = 'Sushrut Patil'
